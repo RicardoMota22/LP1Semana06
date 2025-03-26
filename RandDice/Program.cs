2b34db882ad50497eb633e135fbde60ea61f5512
RicardoMota22 <ricardomota.artist@gmail.com>
2025-03-26 11:46:23 +0000

    RandDice Parse args

---
+++ RandDice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RandDice
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Insert the proper values");
+            }
+
+            //Parse values from console from string to int
+            int seed = int.Parse(args[0]);
+            int start = int.Parse(args[1]);
+
+            int roll = 0;
+            Random random = new Random(seed);
+            
+        }
+    }
+}
