675352a5e08c456b84dc1352488e25998fb48fc5
RicardoMota22 <ricardomota.artist@gmail.com>
2025-03-26 11:19:09 +0000

    Test Different Seeds + See if they alter Instances

--- RandomTest/Program.cs
+++ RandomTest/Program.cs
@@ -13,6 +13,20 @@ namespace RandomTest
             Console.WriteLine();
             for (int i = 0; i < 4; i++)
                 Console.WriteLine(r2.Next());
+            Console.WriteLine();
+            
+            //Show changing one doesnt change the other
+            r1 = new Random(23);
+            for (int i = 0; i < 4; i++)
+                Console.WriteLine(r1.Next());
+            Console.WriteLine();
+            
+            //Stand in for r2
+            Random r3 = new Random(22);
+            for (int i = 0; i < 4; i++)
+                Console.WriteLine(r3.Next());
+            Console.WriteLine();
+
         }
     }
 }
