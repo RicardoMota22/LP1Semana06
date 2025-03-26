4b0e9fe98a1106da3eb72442245fac661cb8912d
RicardoMota22 <ricardomota.artist@gmail.com>
2025-03-26 10:00:00 +0000

    Add Chronos crono2

--- Chronos/Program.cs
+++ Chronos/Program.cs
@@ -18,10 +18,19 @@ namespace Chronos
             //Pause upon 0.5 seconds (500 miliseconds)
             Thread.Sleep(500); 
 
-            
+            //count crono2
+            crono2.Start();
+            Thread.Sleep(250);
             
             //Stop timers
             crono1.Stop();
+            crono2.Stop();
+
+            // Get the elapsed time as a TimeSpan value.
+            TimeSpan ts = crono1.Elapsed;
+            TimeSpan ts2 = crono2.Elapsed;
+
+            
 
 
         }
