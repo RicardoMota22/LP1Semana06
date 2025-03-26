47e8fc7d4e5309952863b7aeb51fd5ecc97c3753
RicardoMota22 <ricardomota.artist@gmail.com>
2025-03-26 09:52:48 +0000

    Add Chronos crono1

---
+++ Chronos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Chronos
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Declare and instance  
+            Stopwatch crono1 = new Stopwatch();
+            Stopwatch crono2 = new Stopwatch();
+
+            //Count
+            crono1.Start();
+
+            //Pause upon 0.5 seconds (500 miliseconds)
+            Thread.Sleep(500); 
+
+            
+            
+            //Stop timers
+            crono1.Stop();
+
+
+        }
+    }
+}
