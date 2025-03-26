2133f9b56abe107864bc9d8e53d6ef56789938ec
RicardoMota22 <ricardomota.artist@gmail.com>
2025-03-26 10:25:16 +0000

    Add Chronos 4DP

--- Chronos/Program.cs
+++ Chronos/Program.cs
@@ -16,19 +16,32 @@ namespace Chronos
             crono1.Start();
 
             //Pause upon 0.5 seconds (500 miliseconds)
-            Thread.Sleep(500); 
+            Thread.Sleep(500);
+            //Stop timer
+            crono1.Stop();
 
             //count crono2
             crono2.Start();
             Thread.Sleep(250);
             
-            //Stop timers
-            crono1.Stop();
+            //Stop timer2
             crono2.Stop();
 
             // Get the elapsed time as a TimeSpan value.
             TimeSpan ts = crono1.Elapsed;
             TimeSpan ts2 = crono2.Elapsed;
+
+            // Format and display the TimeSpan value (4DP).
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            
+            string elapsedTime2 = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts2.Hours, ts2.Minutes, ts2.Seconds,
+                ts2.Milliseconds / 10);
+            
+            Console.WriteLine($"RunTime = {ts.TotalSeconds:F4}");
+            Console.WriteLine($"RunTime = {ts2.TotalSeconds:F4}");
 
             
 
