fb0ae1416823458a5fb7eea3c23f0feab9ee988b
RicardoMota22 <ricardomota.artist@gmail.com>
2025-03-27 21:07:30 +0000

    RandDice Done

--- RandDice/Program.cs
+++ RandDice/Program.cs
@@ -12,12 +12,19 @@ namespace RandDice
             }
 
             //Parse values from console from string to int
-            int seed = int.Parse(args[0]);
-            int start = int.Parse(args[1]);
+            int start = int.Parse(args[0]);
+            int seed = int.Parse(args[1]);
 
             int roll = 0;
             Random random = new Random(seed);
-            
+
+            //Throws the amount of dice and calculates amount
+            for (int i = 0; i < start; i++)
+            {
+                roll += random.Next(1, 7); //1 to 6 dice
+            }
+            Console.WriteLine(roll);
+
         }
     }
 }
