45a09b073cb61b71c6592b7219ca9a9d7cdddf95
RicardoMota22 <ricardomota.artist@gmail.com>
2025-03-27 23:14:55 +0000

    Exercise 9. Created Methods

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -11,18 +11,32 @@ namespace MyGame
         private float health;
         private float shield;
 
-        // Construtor
+        // Construtor (same name as class)
         public Enemy(string name)
         {
             this.name=name; // Porque razao usamos "this", like self in python?
+            
             health =100;
             shield = 0;
         }
 
+        //Different Names from class
         public string GetName()
         {
             return name;
         }
+
+         public float GetHealth()
+        {
+            return health;
+        }
+
+
+        public float GetShield()
+        {
+            return shield;
+        }
+        
 
         public void TakeDamage(float damage)
         {
@@ -35,6 +49,21 @@ namespace MyGame
                 if (health< 0) health = 0;
             }
         }
+       
+       public void SetName(string newname)
+       {
+            if (newname.Length > 8)
+            {
+                int maxLength = 8;
+                string limitedString = new string(newname.Take(maxLength).ToArray());
+            }
+            else
+            {
+                name = newname;
+            }
+       }
+
+        
 
     }
 }
--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -7,12 +7,7 @@ namespace MyGame
     {
         private static void Main(string[] args)
         {
-        // Creating an instance of the Enemy class
-        Enemy enemy1 = new Enemy("Orc");
-        Enemy enemy2 = new Enemy("Goblin");
-
-        Console.WriteLine("Enemies created successfully!");
-        Console.WriteLine(enemy1);
+        
         }
     }
 }
