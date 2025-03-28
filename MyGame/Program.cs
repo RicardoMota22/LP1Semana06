982a18b283545564b3c30443e4487a65d390a92f
RicardoMota22 <ricardomota.artist@gmail.com>
2025-03-28 08:45:57 +0000

    Exercise 10, Add Enemy

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -15,7 +15,19 @@ namespace MyGame
             //Console.WriteLine ($"Name is {loki.GetName()} and HP is {loki.GetHealth()}");
 
             int size = int.Parse(args[0]);
-            string[] array = new string[size];
+            Enemy[] array = new Enemy[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write($"Nome do inimigo {i + 1}:");
+                string name = Console.ReadLine();
+                array[i] = new Enemy(name);
+            }
+
+            foreach (Enemy name in array)
+            {
+                Console.WriteLine($"{name.GetName()} {name.GetHealth()} {name.GetShield()} ");
+            }
 
 
         }
