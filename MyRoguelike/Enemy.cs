b051452c8128ee6a2401f4d000190006d2162f29
RicardoMota22 <ricardomota.artist@gmail.com>
2025-03-28 17:57:53 +0000

    Exercicio de Avaliação

--- MyRoguelike/Enemy.cs
+++ MyRoguelike/Enemy.cs
@@ -1,4 +1,5 @@
 using System;
+using Humanizer;
 
 namespace MyRoguelike
 {
@@ -10,10 +11,51 @@ namespace MyRoguelike
         Tough, 
         Nightmare
     }
-    public class Enemy1
+    public class Enemy
     {
         private string name;
-        private float health;
-        private float shield;
+    
+        public Enemy(string name)
+            {
+                this.name = name;
+            }
     }
+    public class Level
+    {
+        private string name;
+        private int room;
+
+        private string difficulty;
+
+        public Level(int roomcount, string difficulty)
+        {
+            this.room = roomcount;
+            this.difficulty = difficulty;
+        }
+        public void SetEnemyInRoom(int room, string newname)
+        {
+            //int roomplace = int.Parse(args[0]);
+            
+        }
+        public string GetToughness()
+        {
+            return difficulty;
+        }
+        public string GetNumRooms()
+        {
+            string rooms = Convert.ToString(room);
+            return rooms;
+        }
+        public string GetNumEnemies()
+        {
+            return name;
+        }
+        public void PrintEnemies(Level name, int room)
+        {
+            room.ToWords();
+            
+            Console.WriteLine($"{room}: {name}");
+        }
+    }
+
 }
